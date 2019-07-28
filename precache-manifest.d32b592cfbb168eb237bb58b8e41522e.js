a84d4b471ebffb23043b35864ac095e7c030ffe6
Kevin Bai <lkevin957@gmail.com>
2019-07-28 15:54:51 -0700

    Updates

---
+++ precache-manifest.d32b592cfbb168eb237bb58b8e41522e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3e4dc1bbf6a61f5613f3fa52d794d1",
+    "url": "/Chameleon/index.html"
+  },
+  {
+    "revision": "79fed0418e5fca279b1e",
+    "url": "/Chameleon/static/css/main.39fcc386.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/Chameleon/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "79fed0418e5fca279b1e",
+    "url": "/Chameleon/static/js/main.8cdeb102.chunk.js"
+  },
+  {
+    "revision": "1b16b8225fc1be72f280",
+    "url": "/Chameleon/static/js/runtime~main.777bb1d2.js"
+  }
+]);
