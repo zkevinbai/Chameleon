063279e27c53a7b9baa5bc5b7e10a318264f1fdc
Kevin Bai <lkevin957@gmail.com>
2019-07-28 15:02:27 -0700

    Add listeners for state change to colors

--- src/App.js
+++ src/App.js
@@ -2,23 +2,45 @@ import React from 'react';
 import './App.css';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorR: "100",
+      colorG: "100",
+      colorB: "100",
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <div className="colors">
           <div className="color-components">
-            <div className="color-r">
-              Red
+            <div 
+              className="color-r"
+              style={{ 'background-color': `rgb(${this.state.colorR}, 0, 0)` }}
+            >
+              Red: {this.state.colorR}
             </div>
-            <div className="color-g">
-              Green
+            <div 
+              className="color-g"
+              style={{ 'background-color': `rgb(0, ${this.state.colorG}, 0)` }}
+            >
+              Green: {this.state.colorG}
             </div>
-            <div className="color-b">
-              Blue
+            <div 
+              className="color-b"
+              style={{ 'background-color': `rgb(0, 0, ${this.state.colorB})` }}
+            >
+              Blue: {this.state.colorB}
             </div>
           </div>
 
-          <div className="color-rgb">
+          <div
+            className="color-rgbb"
+            style={{ 'background-color': `rgb(${this.state.colorR}, ${this.state.colorG}, ${this.state.colorB})` }}
+          >
             RGB
           </div>
         </div>
